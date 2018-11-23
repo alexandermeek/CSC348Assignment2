f33b54db77a99dc2e0dfc647a0918d34db455a6d
MEEK A. (911454) <911454@Swansea.ac.uk>
2018-11-23 10:16:45 +0000

    Add framework models for Post,User and Comments.

---
+++ CSC348Assignment2/Models/Post.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSC348Assignment2.Models
+{
+    public class Post
+    {
+        [Key, Required]
+        public int Id { get; set; }
+
+        [Required, StringLength(10)]
+        public string Title { get; set; }
+
+        [Required, StringLength(300)]
+        public string Body { get; set; }
+
+        [Required]
+        public User Poster { get; set; }
+
+        [Display(Name = "Posted On")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
+        public DateTime Date
+        {
+            get
+            {
+                return Date;
+            }
+            set
+            {
+                Date = DateTime.Now;
+            }
+        }
+
+        public Comment[] Comments { get; set; }
+    }
+}
---
+++ CSC348Assignment2/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSC348Assignment2.Models
+{
+    public class User
+    {
+        [Key, Required]
+        public int Id { get; set; }
+
+        [Required, StringLength(15)]
+        public string Username { get; set; }
+
+        public 
+    }
+}
